a224bc2baa63399c14aab10fd0c86b89725669a2
Morin Sultan <sultanmorin2001@gmail.com>
2026-05-25 10:19:46 +0100

    refactor: improve layout and animation handling in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
+/* eslint-disable no-unused-vars */
 import Header from "./components/Header";
 import ResultsModal from "./components/ResultsModal";
 import StatsBar from "./components/StatsBar";
 import TypingArea from "./components/TypingArea";
-import {useTypingTest} from "./utils/typingtest";
+import { useTypingTest } from "./utils/typingtest";
+import { AnimatePresence, motion } from "framer-motion";
 
 function App() {
   const typingTest = useTypingTest();
-  const {showResults} = typingTest;
+  const { showResults } = typingTest;
   return (
-    // m-4 lg:mt-8 lg:mx-28
-    <main className="transition-all duration-300 ease-in-out">
+    <main className="h-svh">
       <Header {...typingTest} />
 
-      {!showResults && (
-        <>
-          <StatsBar {...typingTest} />
-          <TypingArea {...typingTest} />
-        </>
-      )}
-
-      {showResults && <ResultsModal {...typingTest} />}
+      <AnimatePresence mode="wait">
+        {!showResults ? (
+          <motion.div
+            key="typing"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            <StatsBar {...typingTest} />
+            <TypingArea {...typingTest} />
+          </motion.div>
+        ) : (
+          <motion.div
+            key="results"
+            initial={{ opacity: 0, scale: 0.95 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            <ResultsModal {...typingTest} />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </main>
   );
 }
